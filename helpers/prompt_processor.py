f68b44e42881aded7540699a35e2510418479b1d
Alcides Ojeda <alcides.ojeda@mail.com>
2023-10-28 17:57:54 +0200

    refactoring 2

--- helpers/prompt_processor.py
+++ helpers/prompt_processor.py
@@ -2,6 +2,7 @@ import os
 from typing import List, Dict
 from models.article import Article
 from models.prompt_command_factory import PromptCommandFactory
+from helpers.openai_handler import OpenAIHandler
 class ProcessPrompts:
     def __init__(self, openai_handler, record_id):
         self.openai_handler = openai_handler
@@ -12,11 +13,11 @@ class ProcessPrompts:
         self.retries = int(os.getenv("OPENAI_RETRIES", 3))
 
     def process(self, prompts: List[Dict], article: Article) -> List[Dict]:
+        openai_handler = OpenAIHandler()
         for index, prompt in enumerate(prompts, start=1):
             prompt_type = prompt.get("type", "").lower().strip()
             command = PromptCommandFactory.create_command(prompt_type)
-            response = command.execute(prompt, self.retries, article=article)
-            prompt["response"] = response
+            command.execute(prompt, self.retries, article=article, openai_handler=openai_handler)
             if self.metadata and index == 2:
                 self.update_metadata(self.record_id, self.metadata)
         return prompts
--- models/prompt_commands.py
+++ models/prompt_commands.py
@@ -1,38 +1,50 @@
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Dict, Optional
+from models.article import Article
+from helpers.openai_handler import OpenAIHandler
 from helpers.html_utils import (add_p_tags, convert_bullets_to_html,
                                 convert_numbers_to_ol, remove_double_quotes, remove_empty_html_tags)
 
 class PromptCommand(ABC):
     @abstractmethod
-    def execute(self, prompt: Dict, retries: int, **kwargs) -> str:
-        pass
+    def execute(self, prompt: Dict, retries: int, article: Article,
+                openai_handler: Optional[OpenAIHandler] = None, **kwargs) -> None:
+        openai_handler = openai_handler or OpenAIHandler()
+        if openai_handler is None:
+            raise ValueError("openai_handler cannot be None")
+        prompt_text = prompt.get("prompt")
+        if prompt_text is not None or prompt_text != "":
+            prompt["response"] = openai_handler.prompt(prompt.get("prompt"))
 
 class MetaDataPromptCommand(PromptCommand):
-    def execute(self, prompt: Dict, retries: int, **kwargs) -> str:
-        metadata = kwargs.get('metadata')
-        if metadata is None:
-            raise ValueError("MetaData prompt command requires 'metadata' parameter")
-        # Processing logic for meta prompts
-        return ""
+    def execute(self, prompt: Dict, retries: int, article: Article,
+                openai_handler: Optional[OpenAIHandler] = None, **kwargs) -> None:
+        super().execute(prompt, retries, article, openai_handler, **kwargs)
+        response = prompt.get("response")
+        prompt["response"] = "" #remove this line after implementing the logic
+        #TODO: update the record in Airtable with the metadata
 
 class ImagePromptCommand(PromptCommand):
-    def execute(self, prompt: Dict, retries: int, **kwargs) -> str:
-        image_urls = kwargs.get('image_urls')
-        if image_urls is None:
-            raise ValueError("Image section requires 'image_urls' parameter")
-        # Processing logic for image prompts
-        return ""
+    def execute(self, prompt: Dict, retries: int, article: Article, **kwargs) -> None:
+        image_urls_csv = article.get("image_urls", "")
+        images_urls_list = image_urls_csv.split(",") if image_urls_csv else []
+        image_url = images_urls_list.pop(0) if len(images_urls_list) > 0 else ""
+        if image_url:
+            prompt["response"] = f'<img src="{image_url}"/>'
+
 
 class ExamplePromptCommand(PromptCommand):
-    def execute(self, prompt: Dict, retries: int, **kwargs) -> str:
-        # Processing logic for image prompts
-        return ""
+    def execute(self, prompt: Dict, retries: int, article: Article,
+                openai_handler: Optional[OpenAIHandler] = None, **kwargs) -> None:
+        super().execute(prompt, retries, article, openai_handler, **kwargs)
 
 
 class DefaultPromptCommand(PromptCommand):
-    def execute(self, prompt: Dict, retries: int, **kwargs) -> str:
+    def execute(self, prompt: Dict, retries: int, article: Article,
+                openai_handler: Optional[OpenAIHandler] = None, **kwargs) -> None:
         # Processing logic for image prompts
-        return ""
+        super().execute(prompt, retries, article, openai_handler, **kwargs)
+
+
 
 
