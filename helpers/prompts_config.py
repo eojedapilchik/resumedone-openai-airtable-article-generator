46f3678c6189d9c8c0acf0484766a8001a77a558
Faneva <rafanorenantsoasarobidy@gmail.com>
2024-05-30 16:50:26 +0300

    feat: refactor code and add extraction list feature

--- helpers/prompts_config.py
+++ helpers/prompts_config.py
@@ -19,5 +19,10 @@ prompts_cfg = {
                         f"Generate a list of extracted	 bullet points. Do not make up any bullet point. Simply extract in a structured list exactly what is in the « Experience Content »"
                         f"If « Experience content » has no information, only return a single element in the list which is « [[Job Role]] at [[Name of the Company]] »"
                         f"If « Experience content » has no information and « company name » are both empty, only return a single element in the list which is « [[Job Role]] »"
+                        f"Do not add any introductory sentence or any context. Just generate a JSON object without key, just array of the list. Do not add markdown formatting, spaces. The result must be ready to parse with a json parser.",
+    "transformation_list":   f"We are trying to separate the different sentences or bullet points from the following text :"
+                        f"For the following work experience, he wrote the following information :"
+                        f"Text : [[List of Transformation selection]]"
+                        f"Generate a list of extracted bullet points. Do not make up any bullet point. Simply extract in a structured list exactly what is in the « text »"
                         f"Do not add any introductory sentence or any context. Just generate a JSON object without key, just array of the list. Do not add markdown formatting, spaces. The result must be ready to parse with a json parser."
 }
